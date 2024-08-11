fd9568e2e39daf5265b0b42a4e8a6bd8c7c2adea
Mariana <marianaalmeidafga@gmail.com>
2024-08-11 09:19:11 -0300

    feat: questões verilog finalizadas 2 -> guia 02

---
+++ Guia_02/Guia_0202/Guia_0202D.v
@@ -0,0 +1,51 @@
+/*
+Guia_0202
+Aluno: Mariana Almeida Mendonça
+Matrícula: 863593
+*/
+
+module Guia_0202;
+
+// define data
+    real x = 0.03125; // decimal
+    real xr = 5; // inteiro
+    integer y = 4; // counter
+    reg [4:0] b = 0 ; // binary
+    reg [2:0] br = 0 ; // binary
+
+// actions
+    initial
+        begin : main
+
+            $display ( "====================" );
+            $display ( "Guia_0202 - Questao D" );
+            $display ( "====================" );
+
+            $display ( "Decimal = %f" , x );
+            $display ( "Binario = 0.%5b", b );
+
+            while ( x > 0 && y >= 0 )
+            begin
+                if ( x*2 >= 1 )
+                begin
+                    b[y] = 1;
+                    x = x*2.0 - 1.0;
+                end
+                else
+                begin
+                    b[y] = 0;
+                    x = x*2.0;
+                end // end if
+                
+                y=y-1;
+            end // end while
+
+            br = xr;
+
+            $display ( "====================" );
+            $display ( "Binario Decimal = %4b", b );
+            $display ( "Binario Inteiro = %4b", br );
+            $display ( "Binario Total = %4b.%4b", br, b );
+        end // main
+
+endmodule // Guia_0202
---
+++ Guia_02/Guia_0202/Guia_0202E.v
@@ -0,0 +1,51 @@
+/*
+Guia_0202
+Aluno: Mariana Almeida Mendonça
+Matrícula: 863593
+*/
+
+module Guia_0202;
+
+// define data
+    real x = 0.75; // decimal
+    real xr = 6; // inteiro
+    integer y = 3; // counter
+    reg [3:0] b = 0 ; // binary
+    reg [3:0] br = 0 ; // binary
+
+// actions
+    initial
+        begin : main
+
+            $display ( "====================" );
+            $display ( "Guia_0202 - Questao E" );
+            $display ( "====================" );
+
+            $display ( "Decimal = %f" , x );
+            $display ( "Binario = 0.%5b", b );
+
+            while ( x > 0 && y >= 0 )
+            begin
+                if ( x*2 >= 1 )
+                begin
+                    b[y] = 1;
+                    x = x*2.0 - 1.0;
+                end
+                else
+                begin
+                    b[y] = 0;
+                    x = x*2.0;
+                end // end if
+                
+                y=y-1;
+            end // end while
+
+            br = xr;
+
+            $display ( "====================" );
+            $display ( "Binario Decimal = %4b", b );
+            $display ( "Binario Inteiro = %4b", br );
+            $display ( "Binario Total = %4b.%4b", br, b );
+        end // main
+
+endmodule // Guia_0202
